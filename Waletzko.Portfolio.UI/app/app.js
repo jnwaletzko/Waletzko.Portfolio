1adb62e535b694d0e8928600d3aa291dae8f0d65
Jake Waletzko <jnwaletzko@gmail.com>
2014-09-02 20:50:23 -0500

    Started working on angular app
    ui not testable

---
+++ Waletzko.Portfolio.UI/app/app.js
@@ -0,0 +1,12 @@
+﻿angular.module('PortfolioApp', [
+    'ngResource',
+    'ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/portfoliodetails',
+            {
+                controller: 'PortfolioDetailsController',
+                templateUrl: ''
+            })
+        .otherwise({ redirectTo: '/'})
+}]);
